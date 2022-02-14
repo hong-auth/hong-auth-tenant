f9c99cd31f7097a293492c349af40e4757759812
hong <lun3322@126.com>
2022-02-14 16:11:47 +0800

    更新 租户查询

--- hong-auth-tenant-app/src/main/java/com/hong/tenant/executor/query/TenantPageListQryExe.java
+++ hong-auth-tenant-app/src/main/java/com/hong/tenant/executor/query/TenantPageListQryExe.java
@@ -4,9 +4,15 @@ import com.alibaba.cola.dto.PageResponse;
 import com.hong.ExecutorI;
 import com.hong.dto.TenantPageListQry;
 import com.hong.dto.data.TenantDTO;
+import com.hong.tenant.TenantDO;
 import com.hong.tenant.TenantRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
 
 /**
  * @author chenhongbo
@@ -24,7 +30,14 @@ public class TenantPageListQryExe implements ExecutorI<PageResponse<TenantDTO>, 
      */
     @Override
     public PageResponse<TenantDTO> execute(TenantPageListQry qry) {
-        
-        return null;
+        PageRequest pageRequest = PageRequest.of(qry.getPageIndex(), qry.getPageSize());
+        Page<TenantDO> page = tenantRepository.findByNameOrCodeOrStatus(qry.getName(), qry.getCode(), qry.getStatus(), pageRequest);
+        var list = new ArrayList<TenantDTO>();
+        page.getContent().forEach(m -> {
+            var dto = new TenantDTO();
+            BeanUtils.copyProperties(m, dto);
+            list.add(dto);
+        });
+        return PageResponse.of(list, ((int) page.getTotalElements()), page.getSize(), page.getNumber());
     }
 }
--- hong-auth-tenant-infrastructure/src/main/java/com/hong/tenant/TenantRepository.java
+++ hong-auth-tenant-infrastructure/src/main/java/com/hong/tenant/TenantRepository.java
@@ -1,10 +1,18 @@
 package com.hong.tenant;
 
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 /**
  * @author chenhongbo
  */
-public interface TenantRepository extends PagingAndSortingRepository<TenantDO, String>, QuerydslPredicateExecutor<TenantDO> {
+public interface TenantRepository extends PagingAndSortingRepository<TenantDO, String> {
+    /**
+     * @param name
+     * @param code
+     * @param status
+     * @return
+     */
+    Page<TenantDO> findByNameOrCodeOrStatus(String name, String code, Integer status, Pageable pageable);
 }
