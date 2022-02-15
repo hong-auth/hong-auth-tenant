49c618738960ad29e453df7951100511dcdb251a
hong <lun3322@126.com>
2022-02-15 16:56:03 +0800

    更新 增加审计配置

---
+++ hong-auth-tenant-infrastructure/src/main/java/com/hong/config/MongodbConfig.java
@@ -0,0 +1,12 @@
+package com.hong.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+/**
+ * @author chenhongbo
+ */
+@Configuration
+@EnableMongoAuditing
+public class MongodbConfig {
+}
